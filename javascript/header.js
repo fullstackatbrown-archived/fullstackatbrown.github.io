0ea23ec920ae5844860ac88e370afe981477b8de
Johnny <jkairoy@gmail.com>
2019-11-22 13:33:39 -0500

    header tweaks

--- javascript/header.js
+++ javascript/header.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
+  // prevents multiple successive clicks
+  let ready = true;
 	$('#nav-icon').click(function(){
-		$(this).toggleClass('open');
-    $('#overlay').toggleClass('open');
-    $('#logo').toggleClass('invert')
+    if (ready) {
+      // reset ready status on timer
+      ready = false;
+      setTimeout(function(){
+        ready = true;
+      }, 500);
+  		$(this).toggleClass('open');
+      $('#overlay').toggleClass('open');
+      $('#logo').toggleClass('invert');
+      if ($('#navmenu').hasClass('open')) {
+        $('.navcontent').addClass('hidden');
+        $('.navcontent').removeClass('open');
+        setTimeout(function(){
+          $('#navmenu').removeClass('open');
+          $('.navcontent').removeClass('hidden');
+        }, 200);
+      } else {
+        $('#navmenu').addClass('open');
+        $('.navcontent').addClass('open');
+      }
+    }
 	});
 });
