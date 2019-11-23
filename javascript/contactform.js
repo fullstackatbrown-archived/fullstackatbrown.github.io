ad934a02518d34dc4cd0dad1d58fe4fab192cd86
Johnny <jkairoy@gmail.com>
2019-11-22 22:23:41 -0500

    email form finished

--- javascript/contactform.js
+++ javascript/contactform.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+
   $('#gform').on('submit', function(e) {
     console.log($('#mailingform'))
     $('#mailingform').removeClass('visible');
+    setTimeout(function(){
+      $('.submissionresponse').addClass('visible');
+      $('.emailinput').val('');
+
+      setTimeout(function(){
+        $('.submissionresponse').removeClass('visible');
+        $('#mailingbutton').removeClass('hidden');
+      }, 2000);
+    }, 1000);
   });
 });
