9d1977c7c2dfadd347b302d4c6dee1761039f640
Johnny <jkairoy@gmail.com>
2019-11-23 19:05:59 -0500

    disabled animations for nonchrome

--- javascript/goo.js
+++ javascript/goo.js
@@ -1,4 +1,11 @@
+var cananimate = true;
+
+// Detect if chrome
+var isChrome = !!window.chrome
+
 $(document).ready(function(){
+
+  // Detects if browser can handle animation
   let starttime = new Date();
   var lastLoop = new Date();
   var count = 0;
@@ -9,29 +16,32 @@ $(document).ready(function(){
       var fps = 1000 / (thisLoop - lastLoop);
       total += fps;
       lastLoop = thisLoop;
-      if ((thisLoop.getTime()-starttime.getTime())/1000 >= 2.5) {
+      if ((thisLoop.getTime()-starttime.getTime())/1000 >= 3.5) {
         clearInterval(fpsLoopId);
         if (total/200 < 70) {
           console.log((total/200) + "fps stopping animation")
           $(".blob").each(function(index) {
             $(this).css({'animation': 'none'});
           });
+          cananimate = false;
         } else {
           console.log((total/200) + "fps continuing animation")
         }
       }
   }
 
-  fpsLoopId = setInterval(fpsLoop, 10);
 
-
-  // Goo JQuery animation
-  var radius = 8;
-  let origins = ['440px 600px', '540px 310px', '430px 180px', '330px 445px', '134px 400px'];
-  $(".blob").each(function(index) {
-    $(this).css({'transform-origin': origins[index]});
-    $(this).css({'animation': 'circle 4s linear infinite'});
-  });
+  if (isChrome) {
+    // Test if browser can handle animation
+    fpsLoopId = setInterval(fpsLoop, 10);
+    // Goo JQuery animation
+    var radius = 8;
+    let origins = ['440px 600px', '540px 310px', '430px 180px', '330px 445px', '134px 400px'];
+    $(".blob").each(function(index) {
+      $(this).css({'transform-origin': origins[index]});
+      $(this).css({'animation': 'circle 4s linear infinite'});
+    });
+  }
 
   // TweenMax implementation, saved for possible optimization
 
